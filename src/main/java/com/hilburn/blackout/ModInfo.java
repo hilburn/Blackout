47b41f4ee6b933500a636bfd173d9da3b4f08d23
hilburn <charliepaterson89@gmail.com>
2014-10-13 22:25:55 +0100

    Version update

--- src/main/java/com/hilburn/blackout/ModInfo.java
+++ src/main/java/com/hilburn/blackout/ModInfo.java
@@ -3,5 +3,5 @@ package com.hilburn.blackout;
 public class ModInfo {
 	public static final String MODID = "blackout";
 	public static final String NAME = "Blackout";
-	public static final String VERSION = "0.5.7";
+	public static final String VERSION = "0.6.0";
 }
